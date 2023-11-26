efdfd76f239b9253d2d2069a4233f730e04ca7ac
Ngan LE <ngan.ltkn2911@gmail.com>
2023-11-27 00:21:34 +0100

    styled active for navlink

--- js/main.js
+++ js/main.js
@@ -1,12 +1,13 @@
-// Add active class to the current nav link 
-// let header = document.getElementById('navbarNav'); 
-// let navLink = header.getElementsByClassName('nav-link'); 
-// // console.log(navItem);
-// for (let i = 0; i < navLink.length; i++) {
-//     navItem[i].addEventListener('click', function() {
-//         let current = document.getElementsByClassName('active'); 
-//         current[0].className = current[0].className.replace(" active", ""); 
-//         this.className += " active";
-//         console.log(this)
-//     })
-// }
+// TODO: Get html element by id and by class name
+let getElById = (id) => document.getElementById(id); 
+let getEleByClass = (className) => document.getElementsByClassName(className);
+
+// TODO: Add active class to the current nav-link
+const navLinkItems = document.querySelectorAll(".nav-link");
+console.log(navLinkItems);
+navLinkItems.forEach((navLinkEle) => {
+    navLinkEle.addEventListener("click", () => {
+        document.querySelector(".active")?.classList.remove('active');
+        navLinkEle.classList.add("active");
+    })
+})

78e048cf1a9148eb3eeb482f9ba6e474b1a04c78
Ngan LE <ngan.ltkn2911@gmail.com>
2023-11-26 19:41:59 +0100

    created assets folder

--- js/main.js
+++ js/main.js
@@ -1,12 +1,12 @@
 // Add active class to the current nav link 
-let header = document.getElementById('navbarNav'); 
-let navLink = header.getElementsByClassName('nav-link'); 
-// console.log(navItem);
-for (let i = 0; i < navLink.length; i++) {
-    navItem[i].addEventListener('click', function() {
-        let current = document.getElementsByClassName('active'); 
-        current[0].className = current[0].className.replace(" active", ""); 
-        this.className += " active";
-        console.log(this)
-    })
-}
+// let header = document.getElementById('navbarNav'); 
+// let navLink = header.getElementsByClassName('nav-link'); 
+// // console.log(navItem);
+// for (let i = 0; i < navLink.length; i++) {
+//     navItem[i].addEventListener('click', function() {
+//         let current = document.getElementsByClassName('active'); 
+//         current[0].className = current[0].className.replace(" active", ""); 
+//         this.className += " active";
+//         console.log(this)
+//     })
+// }

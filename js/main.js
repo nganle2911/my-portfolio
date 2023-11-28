17e561d2b7cacac52b5655fc69479a983ca99187
Ngan LE <ngan.ltkn2911@gmail.com>
2023-11-28 16:40:21 +0100

    feature done - active class for nav

--- js/main.js
+++ js/main.js
@@ -1,7 +1,3 @@
-// TODO: Get html element by id and by class name
-let getElById = (id) => document.getElementById(id); 
-let getEleByClass = (className) => document.getElementsByClassName(className);
-
 // TODO: Add active class to the current nav-link
 const navLinkItems = document.querySelectorAll(".nav-link");
 console.log(navLinkItems);
@@ -10,4 +6,4 @@ navLinkItems.forEach((navLinkEle) => {
         document.querySelector(".active")?.classList.remove('active');
         navLinkEle.classList.add("active");
     })
-})
+});

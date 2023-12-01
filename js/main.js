be2b2ac480e63fca48fc951e5705ceca34c3325f
Ngan LE <ngan.ltkn2911@gmail.com>
2023-12-01 22:55:08 +0100

    changed theme

--- js/main.js
+++ js/main.js
@@ -7,3 +7,17 @@ navLinkItems.forEach((navLinkEle) => {
         navLinkEle.classList.add("active");
     })
 });
+
+// TODO: Change theme color 
+const changeTheme = () => {
+    let element = document.body;
+    element.classList.toggle("dark-mode");
+
+    if (element.classList.contains("dark-mode")) {
+        document.getElementById("myIcon").innerHTML = `<i class="bi bi-sun-fill"></i>`;
+        document.getElementById("myLogo").innerHTML = `<img src="./assets/img/white-logo.png" width="45%" />`;
+    } else {
+        document.getElementById("myIcon").innerHTML = `<i class="bi bi-moon-stars-fill"></i>`;
+        document.getElementById("myLogo").innerHTML = `<img src="./assets/img/brown-logo.png" width="45%" />`;
+    }
+}

976fe6c6d90131a164c613264506355416ac87e8
Ngan LE <ngan.ltkn2911@gmail.com>
2023-12-15 16:58:04 +0100

    moved index.html

--- js/main.js
+++ js/main.js
@@ -1,5 +1,3 @@
-const { default: Typed } = require("typed.js");
-
 // TODO: Add active class to the current nav-link
 const navLinkItems = document.querySelectorAll(".nav-link");
 
@@ -17,10 +15,10 @@ const changeTheme = () => {
 
     if (element.classList.contains("dark-mode")) {
         document.getElementById("myIcon").innerHTML = `<i class="bi bi-sun-fill"></i>`;
-        document.getElementById("myLogo").innerHTML = `<img src="../assets/img/white-logo.png" width="45%" />`;
+        document.getElementById("myLogo").innerHTML = `<img src="./assets/img/white-logo.png" width="45%" />`;
     } else {
         document.getElementById("myIcon").innerHTML = `<i class="bi bi-moon-stars-fill"></i>`;
-        document.getElementById("myLogo").innerHTML = `<img src="../assets/img/brown-logo.png" width="45%" />`;
+        document.getElementById("myLogo").innerHTML = `<img src="./assets/img/brown-logo.png" width="45%" />`;
     }
 };
 
